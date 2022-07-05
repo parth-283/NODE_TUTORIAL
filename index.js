606cffabac3d1e3663ee057103bc7a6c5e4246c2
parth-283 <kp2832002@gmail.com>
2022-07-05 16:22:19 +0530

    CRUD with file system

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const path = require("path");
 const color = require("colors");
 
 // Make Simple API
-http
+/* http
   .createServer((req, res) => {
     res.writeHead(200, { "content-Type": "applicationjson" });
     res.write(JSON.stringify(data));
     res.end();
   })
-  .listen(4000);
+  .listen(4000); */
 
 // Command-Line Input and Create File
 // For Run {node index.js add/remove FILENAME FILECONTENT}
-const input = process.argv;
+/* const input = process.argv;
 if (input[2] == "add") {
   fs.writeFileSync(input[3], input[4]);
 } else if (input[2] == "remove") {
   fs.unlinkSync(input[3]);
 } else {
   console.log("invalid output");
-}
+} */
 
 // File Listing Show by using NodeJs
 /* const dirpath = path.join(__dirname,'files')
@@ -37,26 +37,33 @@ fs.readdir(dirpath,(err,files)=>{
 }) */
 
 // CRUD with file system
-const dirpath = path.join(__dirname, "CRUD");
-const filepath = `${dirpath}/crud.txt`;
+/* const dirpath = path.join(__dirname, "CRUD");
+const filepath = `${dirpath}/crud.txt`; */
 
 // **{CREATE}**
 // fs.writeFileSync(filepath,`This is a CRUD File`)
+
 // **{READ}**
 /* fs.readFile(filepath,'utf8',(err,files)=>{
     console.log(files);
 }) */
+
 // **{UPDATE}**
 /* fs.appendFile(filepath,' And File Name is CRUD.txt',(err)=>{
     if(!err){
         console.log("File Is Updated");
     }
 }) */
+
 // **{RENAME}**
 /* fs.rename(filepath, `${dirpath}/Fruit.txt`, (err) => {
   if (!err) {
     console.log("File Is RENAME");
   }
 }); */
+
 // **{DELETE}**
-fs.unlinkSync(`${dirpath}/Fruit.txt`)
+/* fs.unlinkSync(`${dirpath}/Fruit.txt`) */
+
+
+
